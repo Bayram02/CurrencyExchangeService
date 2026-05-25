424995ec3fc42c177fea824cdc8c1cdbf24917cd
Bayram02 <bayramlib68@gmail.com>
2026-05-25 22:49:36 +0200

    Lab1: Basic WCF Service project created

---
+++ CurrencyExchangeService/App_Code/IService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace CurrencyExchangeService
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        double GetExchangeRate(string currencyCode);
+
+        [OperationContract]
+        double ConvertCurrency(string fromCurrency, string toCurrency, double amount);
+
+        [OperationContract]
+        List<string> GetAvailableCurrencies();
+    }
+}
---
+++ CurrencyExchangeService/App_Code/Service.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace CurrencyExchangeService
+{
+    public class Service : IService
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public double GetExchangeRate(string currencyCode)
+        {
+            try
+            {
+                string url = "http://api.nbp.pl/api/exchangerates/rates/A/" + currencyCode + "/?format=json";
+                var response = client.GetStringAsync(url).Result;
+
+                var serializer = new DataContractJsonSerializer(typeof(NbpResponse));
+                var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(response));
+                var result = (NbpResponse)serializer.ReadObject(stream);
+
+                return result.rates[0].mid;
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+
+        public double ConvertCurrency(string fromCurrency, string toCurrency, double amount)
+        {
+            try
+            {
+                double fromRate = 1.0;
+                double toRate = 1.0;
+
+                if (fromCurrency.ToUpper() != "PLN")
+                    fromRate = GetExchangeRate(fromCurrency);
+
+                if (toCurrency.ToUpper() != "PLN")
+                    toRate = GetExchangeRate(toCurrency);
+
+                if (fromRate <= 0 || toRate <= 0)
+                    return -1;
+
+                double amountInPln = amount * fromRate;
+                return amountInPln / toRate;
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+
+        public List<string> GetAvailableCurrencies()
+        {
+            return new List<string>
+            {
+                "USD", "EUR", "GBP", "CHF", "JPY",
+                "CAD", "AUD", "NOK", "SEK", "DKK"
+            };
+        }
+    }
+
+    [System.Runtime.Serialization.DataContract]
+    public class NbpResponse
+    {
+        [System.Runtime.Serialization.DataMember]
+        public string currency { get; set; }
+
+        [System.Runtime.Serialization.DataMember]
+        public string code { get; set; }
+
+        [System.Runtime.Serialization.DataMember]
+        public List<NbpRate> rates { get; set; }
+    }
+
+    [System.Runtime.Serialization.DataContract]
+    public class NbpRate
+    {
+        [System.Runtime.Serialization.DataMember]
+        public string no { get; set; }
+
+        [System.Runtime.Serialization.DataMember]
+        public string effectiveDate { get; set; }
+
+        [System.Runtime.Serialization.DataMember]
+        public double mid { get; set; }
+    }
+}
